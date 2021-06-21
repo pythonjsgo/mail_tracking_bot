e546ac9f5b4db89c1061245a8bfd175e5527b4c4
Alexander <pythonjsgo@gmail.com>
2021-06-21 05:18:24 +0300

    New handlers added to dispatcher

--- main.py
+++ main.py
@@ -3,19 +3,23 @@ from telegram import ReplyKeyboardMarkup, ReplyKeyboardRemove, Update
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 
 import config
-from handlers import StartHandler, AddDeparture
-
+from db_manager import DB_manager
+from handlers import StartHandler, AddDeparture, MyDepartures, Other
 
 logging.basicConfig(level=logging.DEBUG,
                     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 logger = logging.getLogger(__name__)
 
+db_manager = DB_manager()
 
 def main():
     updater = Updater(config.TOKEN, use_context=True)
     dispatcher = updater.dispatcher
     dispatcher.add_handler(CommandHandler('start', StartHandler))
     dispatcher.add_handler(MessageHandler(Filters.regex("Добавить отправление ➕"), AddDeparture))
+    dispatcher.add_handler(MessageHandler(Filters.regex("Назад ◀️"), StartHandler))
+    dispatcher.add_handler(MessageHandler(Filters.regex("Мои отправления ✉"), MyDepartures))
+    dispatcher.add_handler(MessageHandler(Filters.text, Other))
 
     updater.start_polling()
 
